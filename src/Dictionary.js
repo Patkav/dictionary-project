0cfa631a84098abe9214344fab7f76bbcfea1023
Patkav <patricia.kavanaugha@gmail.com>
2023-11-30 21:51:26 -0500

    Added Dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    alert(`Searching for ${keyword} definition`);
+    let apiUrl =
+      "https://api.shecodes.io/dictionary/v1/define?word=book&key=4e73714c1tadb83363cf2o8c24a08c12";
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
